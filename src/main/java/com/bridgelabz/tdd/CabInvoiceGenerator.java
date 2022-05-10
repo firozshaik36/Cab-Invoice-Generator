74af184afc75237cdf7fdf9f3a429d490f9bbe89
firozshaik36 <firozshaik0007@gmail.com>
2022-05-10 16:14:10 +0530

    Added:Calculating Fare By Given Distance and Time.

--- src/main/java/com/bridgelabz/tdd/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/tdd/CabInvoiceGenerator.java
@@ -1,17 +1,33 @@
 package com.bridgelabz.tdd;
 
+/**
+ * Calculating Fare By given Distance and Time
+ *
+ * @author firoz
+ *
+ * Date:10-05-2022
+ *
+ *
+ */
+
+// Created a Class
 public class CabInvoiceGenerator {
 
+        // Private Variables
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_FARE = 5;
 
+    //Manually generated This Method From TestCase
     public double calculateFare(double kilometers, int time) {
 
-        double totalFare = kilometers*MINIMUM_COST_PER_KILOMETER + time*COST_PER_TIME;
-        if(totalFare<MINIMUM_FARE)
+        double fare = kilometers*MINIMUM_COST_PER_KILOMETER + time*COST_PER_TIME;
+
+        //If Condition to Check the Fare is Minimum or it returns Fare
+        if(fare<MINIMUM_FARE) {
             return MINIMUM_FARE;
-        return totalFare;
+        }
+        return fare;
 
     }
 }
--- src/test/java/com/bridgelabz/tdd/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/tdd/CabInvoiceGeneratorTest.java
@@ -1,14 +1,17 @@
 package com.bridgelabz.tdd;
 
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class CabInvoiceGeneratorTest extends TestCase {
+//Class For Test Cases
 
+public class CabInvoiceGeneratorTest {
+
+    //Test Case
     @Test
     public void givenDistanceAndTimeShouldReturnTheTotalFareForTheJourney(){
 
+        // Created Object
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double fare = cabInvoiceGenerator.calculateFare(2,8);
         Assert.assertEquals(28,fare,0.0);
